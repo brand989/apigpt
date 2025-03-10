32b6d68b3454fdf513e676b7b90371203a40372d
brand989 <brand989@gmail.com>
2025-03-10 10:41:26 +0100

    убрал  лог MONGO_URI перед подключением

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    console.log("🔍 MONGO_URI:", process.env.MONGO_URI); // Выводим MONGO_URI перед подключением
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
